6455f922fc931fdc19f5d0613822d19808dd6b8a
Soumyashree Sahoo <121792834+codewithsoumyashree@users.noreply.github.com>
2023-02-08 23:06:29 +0530

    Add files via upload

---
+++ pg5.java
@@ -0,0 +1,10 @@
+/* to convert from .c to .f*/
+class e
+{
+public static void main(String args[])
+{
+double c=34;
+double f=(c*9/5)+32;
+System.out.println(f+"-F");
+}
+}
---
+++ pg6.java
@@ -0,0 +1,12 @@
+/* to check even or odd*/
+class f
+{
+public static void main(String args[])
+{
+int p=687;
+if(p%2==0)
+System.out.println("THE NUMBER IS EVEN");
+else
+System.out.println("THE NUMBER IS ODD");
+}
+}
