ceaa9747c4c76271e154a6fc31093d7950b91637
Soumyashree Sahoo <121792834+codewithsoumyashree@users.noreply.github.com>
2023-02-08 23:06:10 +0530

    Add files via upload

---
+++ pg3.java
@@ -0,0 +1,11 @@
+/* program to add 2 integers using command line argument*/
+class c
+{
+public static void main(String args[])
+{
+int x=Integer.parseInt(args[0]);
+int y=Integer.parseInt(args[1]);
+int s=x+y;
+System.out.println("Sum is"+s);
+}
+}
---
+++ pg4.java
@@ -0,0 +1,13 @@
+/*to display sum 1 to 10*/
+class d
+{
+public static void main(String args[])
+{
+int s=0;
+for(int i=1;i<=10;i++)
+{
+s+=i;
+}
+System.out.println("THE SUM FROM 1 TO 10 =" + s);
+}
+}
